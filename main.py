e9b71bef346c3e3da1ff2090ceebbc4f00dc2b0c
Stacy-Sanders <stacy.sanders081@gmail.com>
2023-01-14 16:09:45 -0500

    scoreboard and point functions in scoreboard class and add if statements to add points in main

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
-screen.screensize(canvwidth=800, canvheight=600, bg="black")
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
 screen.title("Pong")
 screen.tracer(0)
 
@@ -12,6 +14,8 @@ right_paddle = Paddle((350, 0))
 left_paddle = Paddle((-350, 0))
 
 ball = Ball()
+
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(right_paddle.up, "Up")
@@ -21,10 +25,27 @@ screen.onkey(left_paddle.down, "s")
 
 game_is_on = True
 while game_is_on:
-    screen.update()
     time.sleep(0.1)
+    screen.update()
+    ball.move()
 
+    # Detect collision with the top and bottom
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
 
+    # Detect collision with paddles
+    if ball.distance(right_paddle) < 50 and ball.xcor() > 320 or ball.distance(left_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # left paddle point
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # right paddle point
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 
 screen.exitonclick()
